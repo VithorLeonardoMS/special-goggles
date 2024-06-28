ca1fff8e5a569d82852fac5daff95974538598dc
VithorLeonardoMS <162487463+VithorLeonardoMS@users.noreply.github.com>
2024-06-28 14:58:46 -0300

    Add files via upload

---
+++ AulasUC4/27.06.24/index.js
@@ -0,0 +1,58 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Produto_1 = require("./Produto");
+var venda_1 = require("./venda");
+/*const lista:Array<Produto> = []
+let tempProdut = new Produto('Smart TV sangsung v2 master j6 plus prime s12+ LGBTV',1500,'Sangsung','Televisor',2200)
+lista.push(tempProdut)
+ tempProdut = new Produto('Ifome XRS',1000,'Apou','Celular',10000)
+lista.push(tempProdut)
+tempProdut = new Produto('Le antigo RTXP 4090',1500,'Le antigo','Notebook',2100)
+lista.push(tempProdut)
+*/
+/*function anilizarLista(list:Array<Produto>,parametro:any,filtro:any):any{
+    for(let i:number=list.length-1;list.length>=0;i--){
+       switch(filtro){
+       case 'nome':
+        
+        if(list[i].nome === parametro){
+            return list[i]
+        }
+
+       break
+
+       case 'preço maximo':
+        if(list[i].precoVenda < parametro){
+            return list[i]
+        }
+       break
+
+       case 'preço mínimo':
+        if(list[i].precoVenda >parametro){
+            return list[i]
+        }
+
+        break
+
+        case 'categoria':
+
+        if(list[i].categoria === parametro){
+            return list[i]
+        }
+        break
+       }
+    }
+}
+
+anilizarLista(lista,'celular','categoria').getProduto()
+Deu errado
+*/
+var pdt1 = new Produto_1.Produto('Smart TV sangsung v2 master j6 plus prime s12+ LGBTV', 1500, 'Sangsung', 'Televisor', 2200);
+var pdt2 = new Produto_1.Produto('Ifome XRS', 1000, 'Apou', 'Celular', 10000);
+var pdt3 = new Produto_1.Produto('Le antigo RTXP 4090', 1000, 'Le antigo', 'Notebook', 2000);
+console.log('O lucro do produto 1 é', pdt1.lucroProduto());
+pdt2.getProduto();
+var venda1 = new venda_1.Venda(pdt3, 2, '12/12/12');
+console.log('O total das vendas n1 é: R$', venda1.valorTotal);
+venda1.aplicarDesconto();
+console.log('O valor total das vendas n1 depois do desconto aplicado é: R$', venda1.valorTotal);
---
+++ AulasUC4/27.06.24/venda.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Venda = void 0;
+var Venda = /** @class */ (function () {
+    function Venda(produto, quantidade, data) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.data = data;
+        this.valorTotal = this.calculaTotal();
+    }
+    Venda.prototype.calculaTotal = function () {
+        var y = this.produto.precoVenda;
+        var x = this.quantidade;
+        return x * y;
+    };
+    Venda.prototype.aplicarDesconto = function () {
+        var x = this.produto.lucroProduto() / 2;
+        var y = this.produto.precoVenda;
+        this.valorTotal = (y - x) * this.quantidade;
+    };
+    Venda.prototype.getVenda = function () {
+        console.log("\n    ______________________________\n    Produto: ".concat(this.produto, "\n    Quantidade: ").concat(this.quantidade, "\n    Valor total: ").concat(this.valorTotal, "\n    Data: ").concat(this.data, "\n    ______________________________\n    "));
+    };
+    return Venda;
+}());
+exports.Venda = Venda;
